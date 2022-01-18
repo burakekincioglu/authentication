1857e07f04f9ac8d8f6e38b620446b0cf6ce6436
burakekincioglu <burakekincioglu93@gmail.com>
2022-01-18 15:10:36 +0300

    firebase configuration added

--- App.js
+++ App.js
@@ -6,13 +6,28 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, {Component} from 'react';
 import {Text, View} from 'react-native';
-import { Component } from 'react';
-
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 
 
 class App extends Component {
+
+  UNSAFE_componentWillMount() {
+      firebase.initializeApp(
+        {
+          apiKey: 'AIzaSyBWpYcoUOR1GdQ7eGcrXkZW5VqgULLVQ4U',
+          projectId: 'kimlikdogrulama-544b6',
+          authDomain: 'kimlikdogrulama-544b6.firebaseapp.com',
+          databaseURL: 'https://kimlikdogrulama-544b6.firebaseio.com',
+          storageBucket: 'gs://kimlikdogrulama-544b6.appspot.com', // sorun çıkarsa buraya bak !!!
+          messagingSenderId: '595911092266'
+        }
+      );
+  };
+
   render() {
     return (
       <View>
