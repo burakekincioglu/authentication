e1e7143024e02c08254ba71aba748d658d08591d
burakekincioglu <burakekincioglu93@gmail.com>
2022-01-19 10:11:32 +0300

    LoginForm style eklendi

--- src/LoginFrom.js
+++ src/LoginFrom.js
@@ -1,11 +1,11 @@
 import React, {Component} from 'react';
-import {View, TextInput, Button} from 'react-native';
+import {View, TextInput} from 'react-native';
 import Button from './components/Button';
 
 class LoginForm extends Component {
     state = {email: '', password: ''};
     render(){
-        const {containerStyle, subContainerStyle} = styles;
+        const {containerStyle, subContainerStyle, inputStyle} = styles;
         return(
             <View style={containerStyle}>
                 <View style={subContainerStyle}>
@@ -35,6 +35,29 @@ class LoginForm extends Component {
     
 }
 const styles = {
+    containerStyle:{
+        borderWidth: 1,
+        borderRadius: 2,
+        bordorColor: '#ddd',
+        bordorBottomWidth: 0,
+        shadowColor: '#000',
+        shadowOffset: {width: 0, height: 2},
+        shadowOpacity: 0.1,
+        shadowRadius: 2,
+        elevation: 1,
+        marginLeft: 5,
+        marginRight: 5,
+        marginTop: 10,
+    },
+    subContainerStyle: {
+        borderBottomWidth: 1,
+        padding: 5,
+        backgraoundColor: '#fff',
+        justifyContent: 'flex-start',
+        flexDirection: 'row',
+        borderColor: '#ddd',
+        position: 'relative'
+    },
 
     inputStyle: {
         paddingRight: 5,
